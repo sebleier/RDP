a131657f2eb707e1c3f314bdaf3b3fd208ea14c9
Sean Bleier <sbleier@counsyl.com>
2019-10-30 09:12:54 -0700

    Cleanup the format.

--- __init__.py
+++ __init__.py
@@ -2,27 +2,29 @@
 The Ramer-Douglas-Peucker algorithm roughly ported from the pseudo-code provided
 by http://en.wikipedia.org/wiki/Ramer-Douglas-Peucker_algorithm
 """
-
 from math import sqrt
+
 
 def distance(a, b):
     return  sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
+
 
 def point_line_distance(point, start, end):
     if (start == end):
         return distance(point, start)
     else:
         n = abs(
-            (end[0] - start[0]) * (start[1] - point[1]) - (start[0] - point[0]) * (end[1] - start[1])
+            (end[0] - start[0]) * (start[1] - point[1]) -
+            (start[0] - point[0]) * (end[1] - start[1])
         )
         d = sqrt(
             (end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2
         )
         return n / d
 
+
 def rdp(points, epsilon):
-    """
-    Reduces a series of points to a simplified version that loses detail, but
+    """Reduces a series of points to a simplified version that loses detail, but
     maintains the general shape of the series.
     """
     dmax = 0.0
@@ -32,8 +34,10 @@ def rdp(points, epsilon):
         if d > dmax:
             index = i
             dmax = d
+
     if dmax >= epsilon:
         results = rdp(points[:index+1], epsilon)[:-1] + rdp(points[index:], epsilon)
     else:
         results = [points[0], points[-1]]
+
     return results
